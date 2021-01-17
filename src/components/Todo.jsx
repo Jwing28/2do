42e943ef32a12266963680bcbb9fd6ea4e535400
Jonathan Lee <jonathanlee@Jonathans-MacBook-Pro.local>
2021-01-16 22:37:27 -0600

    Update comments

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
 
 import { Button, Checkbox, Input } from './index';
-// input - type your to do
-// checkbox - check that you've completed it - then a strike through appears or disappears
-// button to delete it - won't be saved.
 
 const Todo = ({ toggleComplete, onDelete, text }) => {
   return (
