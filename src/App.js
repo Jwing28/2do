06a3524f1434cf727da04890e0f3949fade3d3e4
Jonathan Lee <jonathan.wing.lee@gmail.com>
2021-01-17 22:22:28 -0600

    Add project notes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,17 @@ import React from 'react';
 // import logo from './logo.svg';
 // import { Counter } from './features/counter/Counter';
 
+// This week:
+// 1. Fake the db and the db call for now.
+// Focus on what the data looks like and setting up the redux store for the app.
+//  * keep in mind what it should look like for real db.
+// 2. Work on the UI / Routing (pages)
+
+// Weekend or following week
+// 3. Connect to real database
+// 4. Setup authentication.
+// 5. deploy app (should be free).
+// 6. need to keep track of app to maintain it unlike prev. projects.
 import Home from './containers/Home';
 
 import './App.scss';
